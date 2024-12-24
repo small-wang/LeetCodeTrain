9b0462e1fd849ab04861f2219f9919eceffa0891
wangweicjhx <wangwei@cjhxfund.com>
2024-12-24 09:09:42 +0800

    Add

---
+++ src/main/java/com/ww/leetcode/arrayAndString/FindtheIndexoftheFirstOccurrenceinaString28.java
@@ -0,0 +1,17 @@
+package com.ww.leetcode.arrayAndString;
+
+/**
+ * @since 2024/12/23 14:31
+ */
+public class FindtheIndexoftheFirstOccurrenceinaString28 {
+
+    public int strStr(String haystack, String needle) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        FindtheIndexoftheFirstOccurrenceinaString28 findtheIndexoftheFirstOccurrenceinaString28 = new FindtheIndexoftheFirstOccurrenceinaString28();
+        System.out.println(findtheIndexoftheFirstOccurrenceinaString28.strStr("", ""));
+    }
+}
--- src/main/java/com/ww/leetcode/arrayAndString/LongestCommonPrefix14.java
+++ src/main/java/com/ww/leetcode/arrayAndString/LongestCommonPrefix14.java
@@ -6,8 +6,44 @@ package com.ww.leetcode.arrayAndString;
 public class LongestCommonPrefix14 {
 
     public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        char tmp;
+        int i = 0;
+        boolean stop = false;
 
+        while(!stop) {
 
-        return "";
+            if (i >= strs[0].length()) {
+                stop = true;
+            } else {
+                tmp = strs[0].charAt(i);
+                for (int j = 1; j <= strs.length - 1; j++) {
+                    if (i >= strs[j].length()) {
+                        stop = true;
+                        break;
+                    } else {
+                        char strsChar = strs[j].charAt(i);
+                        if (tmp != strsChar) {
+                            stop = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!stop) {
+                    result += tmp;
+                }
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix14 longestCommonPrefix14 = new LongestCommonPrefix14();
+        String[] strs = {"dog","racecar","car"};
+        System.out.println(longestCommonPrefix14.longestCommonPrefix(strs));
     }
 }
