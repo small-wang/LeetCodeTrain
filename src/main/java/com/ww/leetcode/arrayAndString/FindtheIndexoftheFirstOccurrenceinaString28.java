d54698a9a3ce342336180d466eec3ead24fc4d67
wangweicjhx <wangwei@cjhxfund.com>
2024-12-24 14:31:50 +0800

    Add

--- src/main/java/com/ww/leetcode/arrayAndString/FindtheIndexoftheFirstOccurrenceinaString28.java
+++ src/main/java/com/ww/leetcode/arrayAndString/FindtheIndexoftheFirstOccurrenceinaString28.java
@@ -7,11 +7,26 @@ public class FindtheIndexoftheFirstOccurrenceinaString28 {
 
     public int strStr(String haystack, String needle) {
 
-        return 0;
+        if (haystack.length() < needle.length()) {
+            return -1;
+        } else {
+            for (int i = 0; i <= haystack.length() - 1; i++) {
+                if (haystack.length() - i < needle.length()) {
+                    return -1;
+                } else {
+                    String tmp = haystack.substring(i, i + needle.length());
+                    if (tmp.equals(needle)) {
+                        return i;
+                    }
+                }
+            }
+        }
+
+        return -1;
     }
 
     public static void main(String[] args) {
         FindtheIndexoftheFirstOccurrenceinaString28 findtheIndexoftheFirstOccurrenceinaString28 = new FindtheIndexoftheFirstOccurrenceinaString28();
-        System.out.println(findtheIndexoftheFirstOccurrenceinaString28.strStr("", ""));
+        System.out.println(findtheIndexoftheFirstOccurrenceinaString28.strStr("aleetaa", "leetc"));
     }
 }
---
+++ src/main/java/com/ww/leetcode/twopointers/IsSubsequence392.java
@@ -0,0 +1,9 @@
+package com.ww.leetcode.twopointers;
+
+/**
+ * @since 2024/12/24 14:31
+ */
+public class IsSubsequence392 {
+
+    
+}
