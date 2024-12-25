fb90f36b7ba45c6340d4f1f231356d44759f621b
wangweicjhx <wangwei@cjhxfund.com>
2024-12-25 10:35:04 +0800

    Add

---
+++ src/main/java/com/ww/leetcode/twopointers/ContainerWithMostWater11.java
@@ -0,0 +1,16 @@
+package com.ww.leetcode.twopointers;
+
+/**
+ * @since 2024/12/25 9:12
+ */
+public class ContainerWithMostWater11 {
+
+    public int maxArea(int[] height) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ContainerWithMostWater11().maxArea(new int[]{1, 2}));
+    }
+}
